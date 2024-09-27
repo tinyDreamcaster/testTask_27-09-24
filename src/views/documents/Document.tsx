9ad0b9a091ea8d0c4c87db99e9a3e87f8ce580c5
tinyDreamCaster <frunicefrun@gmail.com>
2024-09-27 18:14:09 +0300

    test task

--- src/views/documents/Document.tsx
+++ src/views/documents/Document.tsx
@@ -89,17 +89,6 @@ const Document = (): JSX.Element => {
     document.body.removeChild(element)
   }
 
-  function getBase64(url: any) {
-    axios.get(url).then((res: any) => {
-      const url = window.URL.createObjectURL(new Blob([res.data]))
-      const link = document.createElement('a')
-      link.href = url
-      link.setAttribute('download', 'file.pdf')
-      document.body.appendChild(link)
-      link.click()
-    })
-  }
-
   return (
     <CContainer>
       <CCard>
